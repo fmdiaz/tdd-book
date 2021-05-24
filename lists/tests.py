72c008977289e252ed272813478c270a38865c7c
Martín Díaz <super.martin.diaz@gmail.com>
2021-05-24 11:40:47 -0300

    Fix name of home page template class

--- lists/tests.py
+++ lists/tests.py
@@ -7,7 +7,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """Home page Unit Tests"""
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
         
